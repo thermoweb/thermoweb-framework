4aded9e12964a430c0e1c6abfa4e54670238d8e3
Romain Chauveau <romain.chauveau@praxedo.com>
2023-07-31 14:12:26 +0200

    wip

--- core/src/main/java/org/thermoweb/core/http/JsonBodyHandler.java
+++ core/src/main/java/org/thermoweb/core/http/JsonBodyHandler.java
@@ -26,7 +26,7 @@ public class JsonBodyHandler<W> implements HttpResponse.BodyHandler<W> {
 
         return HttpResponse.BodySubscribers.mapping(
                 upstream,
-                (String body) -> {
+                body -> {
                     try {
                         ObjectMapper objectMapper = new ObjectMapper();
                         objectMapper.registerModule(new JavaTimeModule());
--- core/src/main/java/org/thermoweb/core/utils/RandomUtils.java
+++ core/src/main/java/org/thermoweb/core/utils/RandomUtils.java
@@ -1,8 +1,11 @@
 package org.thermoweb.core.utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.random.RandomGenerator;
 
 public class RandomUtils {
@@ -12,7 +15,30 @@ public class RandomUtils {
         return list.get(generator.nextInt(list.size()));
     }
 
+    public static <T> T getRandomItem(T[] array) {
+        return getRandomItem(Arrays.stream(array).toList());
+    }
+
     public static <K, V> V getRandomItem(Map<K, V> map) {
         return getRandomItem(new ArrayList<>(map.values()));
+    }
+
+    public static <T> List<T> getRandomItems(List<T> list, int numberOfItems) {
+        if (list.size() <= numberOfItems) {
+            return list;
+        }
+
+        List<T> elements = new ArrayList<>(List.copyOf(list));
+        Set<T> randomSet = new HashSet<>();
+        while(randomSet.size() < numberOfItems) {
+            T element = getRandomItem(elements);
+            randomSet.add(element);
+            elements.remove(element);
+        }
+        return randomSet.stream().toList();
+    }
+
+    public static <T> List<T> getRandomItems(T[] array, int numberOfItems) {
+        return getRandomItems(Arrays.stream(array).toList(), numberOfItems);
     }
 }
